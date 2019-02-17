0b2aee0d2c4ec2b51f6adf42de939bc59f0a3ab4
florianvazelle <ponythugflorian@gmail.com>
2019-02-17 01:45:55 +0100

    Correction Opacity Room After Fight

--- game/states/move.lua
+++ game/states/move.lua
@@ -1,78 +1,22 @@
-local sti = require "lib.sti"
-local bump = require "lib.bump"
-
 move = {}
 
 function move.load()
   tx, ty, sx, sy = 0, 0, 1, 1
 
-  local step = love.math.random(9, 15)
-  print(step)
+  move.step = love.math.random(9, 15)
 
-  local world = bump.newWorld(16)
-  map = sti("assets/data/dungeon.lua", {"bump"}) -- Load map file
-  map:bump_init(world)
-
-  -- Get player spawn object
-  --local player
-  if player == nil then
-    for _, object in pairs(map.objects) do
-      if object.name == "Player" then
-        player = Player.new(object.x, object.y)
-        break
-      end
-    end
-  end
-
-  map:addCustomLayer("Sprite Layer", 14) -- Create a Custom Layer
-  -- Add data to Custom Layer
-  local spriteLayer = map.layers["Sprite Layer"]
-  spriteLayer.sprites = {
-    player
-  }
-
-  world:add(player, player.x, player.y, player.w, player.h) -- Add player to world
-
-  function spriteLayer:update(dt)
-    local goalX, goalY = player:update()
-    local actualX, actualY, cols, len = world:check(player, goalX, goalY)
-    world:update(player, actualX, actualY)
-    if not (player.x == actualX and player.y == actualY) then step = step - 1 end
-    player.x, player.y = actualX, actualY
-
-    if step == 0 then
-      change_state(fight)
-    end
-  end
-
-  function spriteLayer:draw()
-    for key, sprite in pairs(self.sprites) do
-      local x = sprite.x
-      local y = sprite.y
-      local r = sprite.r
-      local sx = sprite.sx
-      love.graphics.draw(sprite.image, x, y, r, sx)
-    end
-  end
-
-  map:removeLayer("Spawn Point") -- Remove unneeded object layer
-
-
-  -- define update function for all room layer
   local w = love.graphics.getWidth()
   local h = love.graphics.getHeight()
-
-  for _, layer in pairs(map.layers) do
-    if "Room" == string.sub(layer.name, 0, 4) and not (layer.name == "Rooms") then
-      layer = Room.new(layer, map.objects[layer.name])
-    end
-  end
 
   move.resize(w, h)
 end
 
 function move.update(dt)
   map:update(dt)
+
+  if move.step == 0 then
+    change_state(fight)
+  end
 end
 
 function move.draw()
--- main.lua
+++ main.lua
@@ -7,6 +7,9 @@ require "game.interfaces.room"
 require "game.states.move"
 require "game.states.fight"
 
+local sti = require "lib.sti"
+local bump = require "lib.bump"
+
 local state = nil
 
 function change_state(s)
@@ -15,8 +18,56 @@ function change_state(s)
 end
 
 function love.load()
-  mainFont = love.graphics.newFont("assets/fonts/A Goblin Appears!.otf")
+  local mainFont = love.graphics.newFont("assets/fonts/A Goblin Appears!.otf")
   love.graphics.setFont(mainFont)
+
+  local world = bump.newWorld(16)
+  map = sti("assets/data/dungeon.lua", {"bump"}) -- Load map file
+  map:bump_init(world)
+
+  -- Get player spawn object
+  for _, object in pairs(map.objects) do
+    if object.name == "Player" then
+      player = Player.new(object.x, object.y)
+      break
+    end
+  end
+
+  map:addCustomLayer("Sprite Layer", 14) -- Create a Custom Layer
+  -- Add data to Custom Layer
+  local spriteLayer = map.layers["Sprite Layer"]
+  spriteLayer.sprites = {
+    player
+  }
+
+  world:add(player, player.x, player.y, player.w, player.h) -- Add player to world
+
+  function spriteLayer:update(dt)
+    local goalX, goalY = player:update()
+    local actualX, actualY, cols, len = world:check(player, goalX, goalY)
+    world:update(player, actualX, actualY)
+    if not (player.x == actualX and player.y == actualY) then move.step = move.step - 1 end
+    player.x, player.y = actualX, actualY
+  end
+
+  function spriteLayer:draw()
+    for key, sprite in pairs(self.sprites) do
+      local x = sprite.x
+      local y = sprite.y
+      local r = sprite.r
+      local sx = sprite.sx
+      love.graphics.draw(sprite.image, x, y, r, sx)
+    end
+  end
+
+  map:removeLayer("Spawn Point") -- Remove unneeded object layer
+
+  -- Define update function for all room layer
+  for _, layer in pairs(map.layers) do
+    if "Room" == string.sub(layer.name, 0, 4) and not (layer.name == "Rooms") then
+      layer = Room.new(layer, map.objects[layer.name])
+    end
+  end
 
   change_state(move)
 end
@@ -30,11 +81,4 @@ end
 function love.keypressed(key) state.keypressed(key) end
 function love.resize(w, h) state.resize(w, h) end
 
-function love.focus(f)
-  if not f then
-    print("LOST FOCUS")
-  else
-    print("GAINED FOCUS")
-  end
-  gameIsPaused = not f
-end
+function love.focus(f) gameIsPaused = not f end
